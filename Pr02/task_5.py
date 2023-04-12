02344c8c76cca85af97ae24ed1cd8bce81eb8c16
Alry Line <anastasiashalabay98@gmail.com>
2023-04-12 20:12:59 +0300

    new work

--- Pr02/task_5.py
+++ Pr02/task_5.py
@@ -1,13 +1,11 @@
-from secrets import choice
-
-
 print('Добрий день!')
 a=int(input('Введіть число: '))
 b=int(input('Введіть число: '))
-print('Будь ласка, зробіть вибір: ')
+print('Варіанти')
 print('1: сумма')
 print('2: різниця')
 print('3: добуток')
+choice = int(input('Ваш вибір: '))
 if choice == 1:
     print(a+b)
 elif choice == 2:
