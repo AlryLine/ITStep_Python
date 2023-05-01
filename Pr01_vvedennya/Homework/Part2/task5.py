8f13a33dd9a07cb7868956102938ea5c93972b89
AlryLine <127983296+AlryLine@users.noreply.github.com>
2023-05-01 23:37:31 +0300

    Delete Pr01_vvedennya directory

--- Pr01_vvedennya/Homework/Part2/task5.py
+++
@@ -1,5 +0,0 @@
-print('     "Life is what happens')
-print('             when')
-print("                 you're busy")
-print('                         making other plans"')    
-print('                                          John Lennon.')
--- Pr01_vvedennya/Homework/Part3/task1.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Number: '))
-b = int(input('Number: '))
-c = int(input('Number: '))
-print(a)
-print(b)
-print(c)
-print(a,b,c)
