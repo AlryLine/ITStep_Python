9894693f52b391ac0c284d64bfcb1b9e98eeeecc
AlryLine <anastasiashalabay98@gmail.com>
2023-05-07 02:52:16 +0300

    change tasks

--- Homework/Modul3_Part3_for/task3.py
+++ Homework/Modul3_Part3_for/task3.py
@@ -1,9 +1,9 @@
 count = 0
 for i in range (100, 10000):
-    a = i // 1000
-    b = i // 100 % 10
-    c = i // 10 % 10
-    d = i % 10
+    a = i % 10
+    b = i // 10 % 10
+    c = i // 100 % 10
+    d = i // 1000
     if a != b or a != c or a != d or b != c or b != d or c != d:
         count += 1
-    print (count)
+print (count)
--- Homework/Modul3_Part3_for/task4.py
+++ Homework/Modul3_Part3_for/task4.py
@@ -1,14 +1,17 @@
-count1=0
-count2=0
-for a in range(100,9999):
-    n1=(a//1000)
-    n2=(a//100)%10
-    n3=(a%100)//10
-    n4=a%10
-    if a <1000:
-        (n2!=n3 or n2!=n4 or n3!=n4)
-        count1 += 1
-    if a >=1000:
-        (n1!=n2 or n1!=n3 or n1!=n4 or n2!=n3 or n2!=n4 or n3!=n4)
-        count2 += 1
-        print("Количество разных целых чисел в диапазоне от 100 до 9999:",count1+count2 )
+n = int(input ('Input a number: '))
+raz = n
+count = 0
+rezult = 0
+while raz:
+    count +=1
+    raz//=10
+raz=n
+i=0
+while count:
+    number=raz%10
+    raz//=10
+    if not (number ==3 or number ==6):
+        rezult=rezult+(number*10**i)
+        i+=1
+    count-=1
+print('Rezult =',rezult)
