f0b49a01a88408d0779f7950cc346572848ee00b
AlryLine <anastasiashalabay98@gmail.com>
2023-05-07 01:04:16 +0300

    finished_homework

--- Homework/Modul3_Part2_while/task1.py
+++ Homework/Modul3_Part2_while/task1.py
@@ -4,21 +4,11 @@ sum1 = 0
 sum2 = 0
 sum3 = 0
 while a < b:
-    if a%2 == 0:
-        print (sum1)
-        a += 2
-    elif a%2 != 0:
-        print (sum2)
-        a += 1
-    elif a %9 == 0:
-        print (sum3)
-        a += 1
+    sum1 += a if a%2 else 0
+    sum2 += a if not a%2 else 0
+    sum3 += a if a%9 else 0
     a += 1
 print (sum1)
 print (sum2)
 print (sum3)
 print ((sum1 + sum2 + sum3) // 3)
-
-    #sum1 += a if a%2 else 0
-    #sum2 += a if not a%2 else 0
-    #sum3 += a if a%9 else 0
