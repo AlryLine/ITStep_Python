8102d1a6c29ea9136568122b86aba605313cccc8
Alry Line <anastasiashalabay98@gmail.com>
2023-05-10 20:27:55 +0300

    new_pr5

---
+++ Practic/task1_f.py
@@ -0,0 +1,5 @@
+def print_text ():
+    print('   "Don\'t let the noise of others\' opinions')
+    print('   drown out your own inner voice.”')
+    print('                                 Steve Jobs')
+print_text ()
