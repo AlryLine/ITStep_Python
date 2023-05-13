d9624b1d7fd2dcc3a63dd52d6acba634dea5ec3f
AlryLine <127983296+AlryLine@users.noreply.github.com>
2023-05-13 17:30:56 +0300

    Delete Pr02_operatory_rozgaluzhen directory

--- Pr02_operatory_rozgaluzhen/Homework/Modul_2_Part2/task4.py
+++
@@ -1,9 +0,0 @@
-print ('Welcome!')
-a = int(input('Input number: '))
-b = int(input('Input number: '))
-if a == b:
-    print ('Numbers are equal')
-elif a <= b:
-    print (a, end=' ')
-    a += 1
-print ('Great, bye!')
