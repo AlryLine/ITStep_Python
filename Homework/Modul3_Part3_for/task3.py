9a450e99a41bf1e23f4801b6c1059b63d68d9c65
AlryLine <127983296+AlryLine@users.noreply.github.com>
2023-05-18 18:31:18 +0300

    Delete Homework directory

--- Homework/Modul3_Part3_for/task3.py
+++
@@ -1,9 +0,0 @@
-count = 0
-for i in range (100, 10000):
-    a = i % 10
-    b = i // 10 % 10
-    c = i // 100 % 10
-    d = i // 1000
-    if a != b and a != c and a != d and b != c and b != d and c != d:
-        count += 1
-print (count)
