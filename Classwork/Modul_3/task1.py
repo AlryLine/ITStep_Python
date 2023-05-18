c83e9671d0072b987fb96f37915b10e908b7a2c2
AlryLine <127983296+AlryLine@users.noreply.github.com>
2023-05-18 18:31:32 +0300

    Delete Classwork directory

--- Classwork/Modul_3/task1.py
+++
@@ -1,3 +0,0 @@
-n = int(input('Input number: '))
-for i in range(1, 11):
-    print(n, '*', i, '=', n*i)
--- Classwork/Modul_3/task3.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Введіть перший діапазон: '))
-b = int(input('Введіть другий діапазон: '))
-n = int(input('Введіть число: '))
-while not a<n<=b:
-    n = int(input('Введіть повторно число: '))
-for i in range(a,b+1):
-    print
