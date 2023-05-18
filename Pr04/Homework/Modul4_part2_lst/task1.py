9e215576d3764abb322cc6da1f6eb4f7c1b095e4
AlryLine <anastasiashalabay98@gmail.com>
2023-05-19 00:26:40 +0300

    neewtask

--- Pr04/Homework/Modul4_part2_lst/task1.py
+++ Pr04/Homework/Modul4_part2_lst/task1.py
@@ -1,14 +1,14 @@
 n = input('Введіть арифметичний вираз: ')
 
 if '+' in n:
-    a, b = n.split()
+    a, b = n.split(' + ')
     print(int(a) + int(b))
 elif '-' in n:
-    a, b = n.split()
+    a, b = n.split(' - ')
     print(int(a) - int(b))
 elif '*' in n:
-    a, b = n.split()
+    a, b = n.split(' * ')
     print(int(a) * int(b))
 else:
-    a, b = n.split()
+    a, b = n.split(' // ')
     print(int(a) // int(b))
