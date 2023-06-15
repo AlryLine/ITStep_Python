ceb9cddc6816045326d578a23fe3cc07c9c1b539
AlryLine <anastasiashalabay98@gmail.com>
2023-06-15 18:09:26 +0300

    fin_hw

--- Homework/Modul6_part3_naslid/task1.py
+++ Homework/Modul6_part3_naslid/task1.py
@@ -3,35 +3,57 @@ class Device:
         self.brand = brand
         self.material = material
         self.purpose = purpose
-        self.__dimensions = dimensions
+        self.dimensions = dimensions
         self.price = price
-
-    def get_dimensions (self):
-        return self.__dimensions
-    def put_dimensions (self, dimen):
-        self.__dimensions = dimen
     
     def show_info (self):
-        print (self.brand, self.material, self.purpose, self.__dimensions, self.price, end= ' ')
+        print (self.brand, self.material, self.purpose, self.dimensions, self.price, end= ' ')
         
 class CoffeeMachine (Device):
-    def __init__(self, brand, material, purpose, dimensions, price, control_type):
+    def __init__(self, brand, material, purpose, dimensions, price, control_type, making):
         super().__init__(brand, material, purpose, dimensions, price)
         self.control_type = control_type
-                
-    def get_price (self):
-        return self.price
-    def put_price (self, price):
-        self.price = price          
+        self.making = making
+    
+    def info_about_coffee_machine (self):
+        print ('Інформація про кавомашину:')
+       
+    def __str__ (self):
+        return f' Бренд: {self.brand}\n Матеріал: {self.material}\n Призначення: {self.purpose}\n Габарити: {self.dimensions}\n Ціна: {self.price} грн\n Тип управління: {self.control_type}'
+ 
+    def print_text (self):
+        print ('Приготування кави:')
+    
+    def making_coffee (self, making):
+        if making == 1:
+            print (' Готується еспрессо')
+        elif making == 2:
+            print (' Готується капучіно')
+        elif making == 3:
+            print (' Готується латте')
+        else:
+            print (' Такого номера в меню немає! Будь-ласка, оберіть кавовий напій та натисніть кнопку від 1 до 3!')              
         
     def show_info (self):
         super().show_info()
-        print (self.control_type)
+        print (self.control_type, self.making)
 
 class Blender (Device):
-    def __init__(self, brand, material, purpose, dimensions, price, count_of_speeds):
+    def __init__(self, brand, material, purpose, dimensions, price, count_of_speeds, milk, fruits):
         super().__init__(brand, material, purpose, dimensions, price)
         self.count_of_speeds = count_of_speeds
+        self.milk = milk
+        self.fruits = fruits
+     
+    def info_about_blender (self):
+        print ('Інформація про блендер:')    
+        
+    def make_milk_shake (self, milk, fruits):
+        print ('Готуємо молочний коктель:')
+        print (' Додаємо', milk, 'в блендер')
+        print (' Тепер додаємо', fruits)
+        print (' Змішуємо все разом')
+        print ('Молочний коктель готовий!')            
 
     def show_info (self):
         super().show_info()
@@ -47,24 +69,29 @@ class MeatGrinder (Device):
     def put_info_about_reverse (self, reverse):
         self.__reverse = reverse
         
+    def info_about_meat_grinder (self):
+        print ('Інформація про м\'ясорубку:')    
+        
     def show_info (self):
         super().show_info()
         print (self.__reverse)
 
-blender = Blender ('Бренд: Tefal,', 'Матеріал: метал та пластик,', 'Призначення: подрібнення їжі та збивання напоїв,', 4, 'Ціна: 4 299 грн,', 'Кількість швидкостей: мультишвидкісний.')
-blender.get_dimensions ()
-blender.put_dimensions ('Габарити: 40,6 х 19,2 х 21,5 см,')
-blender.get_dimensions ()
+coffee_machine = CoffeeMachine ('Philips','пластик','приготування кавових напоїв','43,3 х 24,6 x 37,1 cм',35999,'сенсорний.', 7)
+coffee_machine.info_about_coffee_machine ()
+print ((coffee_machine))
+print ()
+coffee_machine.print_text ()
+coffee_machine.making_coffee (3)
+print ()
+blender = Blender (' Бренд: Tefal\n','Матеріал: метал та пластик\n','Призначення: подрібнення їжі та збивання напоїв\n','Габарити: 40,6 х 19,2 х 21,5 см\n','Ціна: 4299 грн\n','Кількість швидкостей: мультишвидкісний', 7, 8)
+blender.info_about_blender ()
 blender.show_info ()
-
-coffee_machine = CoffeeMachine ('Бренд: Philips,', 'Матеріал: пластик,', 'Призначення: приготування кавових напоїв,', 'Габарити: 43,3 х 24,6 x 37,1 cм,', 5, 'Тип управління: сенсорний.')
-coffee_machine.get_price ()
-coffee_machine.put_price ('Ціна: 35 999 грн, зі знижкою - 19 999 грн (-43%),')
-coffee_machine.get_price ()
-coffee_machine.show_info ()
-
-meat_grinder = MeatGrinder ('Бренд: Bosch,', 'Матеріал: пластик,', 'Призначення: подрібнення м\'яса,', 'Габарити: 25 х 16 х 23 см,', 'Ціна: 5 859 грн, зі знижкою - 4 699 грн (-20%),', 6)
+print ()
+blender.make_milk_shake ('молоко', 'банан та полуницю')
+print ()
+meat_grinder = MeatGrinder (' Бренд: Bosch\n','Матеріал: пластик\n','Призначення: подрібнення м\'яса\n','Габарити: 25 х 16 х 23 см\n','Ціна: 5859 грн, зі знижкою - 4699 грн (-20%)\n', 6)
 meat_grinder.get_info_about_reverse ()
 meat_grinder.put_info_about_reverse ('Наявність реверса: так.')
 meat_grinder.get_info_about_reverse ()
+meat_grinder.info_about_meat_grinder ()
 meat_grinder.show_info ()
--- Homework/Modul6_part3_naslid/task2.py
+++ Homework/Modul6_part3_naslid/task2.py
@@ -14,27 +14,38 @@ class Ship:
         print (self.name, self.type_of_ship, self.speed,  self.__displacement_tonnage)
         
 class Frigate (Ship):
-    def __init__(self, name, type_of_ship, speed, displacement_tonnage, battery):
+    def __init__(self, name, type_of_ship, speed, displacement_tonnage, sails, battery):
         super().__init__(name, type_of_ship, speed, displacement_tonnage)
+        self.sails = sails
         self.battery = battery
         
-    def show_info_about_battery (self):
-        print ('Батарея - основний вогневий тактичний підрозділ в артилерії ракетних, зенітних ракетних, ' 
-               'ракетно-артилерійских, а також у берегових військах та надводних кораблях військово-морського флоту.')
+    def info_about_frigate (self):
+        print ('Корабель №1:')    
         
+    def show_info_about_battery (self):
+        print ('Батарея - основний вогневий тактичний підрозділ в артилерії ракетних, зенітних ракетних,\nракетно-артилерійских, а також у берегових військах та надводних кораблях військово-морського флоту.')
+        
+    def raise_the_sails (self, sails):
+        print ('Команда підняти або опустити вітрила фрегату:')    
+        if sails == 'підняти вітрила':
+            print ('Вітрила підняті!')
+        elif sails == 'опустити вітрила':
+            print ('Вітрила опущені!')
+                
     def show_info (self):
         super().show_info()
-        print (self.battery)
+        print (self.sails, self.battery)
         
 class Destroyer (Ship):
     def __init__(self, name, type_of_ship, speed, displacement_tonnage, additions):
         super().__init__(name, type_of_ship, speed, displacement_tonnage)
         self.additions = additions
         
-    def get_speed (self):
-        return self.speed
-    def add_speed (self, s):
-        self.speed = s
+    def info_about_destroyer (self):
+        print ('Корабель №2:')    
+             
+    def __str__(self):
+        return f' Швидкість: до {self.speed} вузлів.'    
         
     def show_info (self):
         super().show_info()
@@ -43,34 +54,40 @@ class Destroyer (Ship):
 class Cruiser (Ship):
     def __init__(self, name, type_of_ship, speed, displacement_tonnage, independence):
         super().__init__(name, type_of_ship, speed, displacement_tonnage)
-        self.independence = independence
+        self.__independence = independence
         
     def get_info (self):
-        return self.independence
+        return self.__independence
     def add_info (self, i):
-        self.independence = i
+        self.__independence = i
+    
+    def info_about_cruiser (self):
+        print ('Корабель №3:')    
         
     def show_info (self):
         super().show_info()
-        print (self.independence)
+        print (self.__independence)
         
-frigate = Frigate ('Модель корабля: Фрегат.', 'Тип корабля: військове судно.', 
-                   'Швидкість: понад 30 вузлів.', 'Водотоннажність: до 4000 тонн.',
-                   'Фрегати мають або тільки одну закриту батарею, або одну закриту та одну відкриту на верхній палубі.')
+frigate = Frigate (' Модель: Фрегат.\n','Тип: військове судно.\n','Швидкість: понад 30 вузлів.\n','Водотоннажність: до 4000 тонн.',' Наявність вітрил: може мати вітрила.','\nОсобливості:\n Фрегати мають або тільки одну закриту батарею, або одну закриту та одну відкриту на верхній палубі.')
+frigate.info_about_frigate ()
 frigate.show_info ()
+print ()
 frigate.show_info_about_battery ()
-
-destroyer = Destroyer ('Модель корабля: Есмінець.', 'Тип корабля: бойовий корабель.',
-                       'Швидкість: до 39 вузлів.', 'Водотоннажність: 1500 - 4500 тонн.',
-                       'Деякі есмінці мають вертолітний майданчик.')
-destroyer.get_speed ()
-destroyer.add_speed ('Швидкість: до 40 вузлів.')
-destroyer.get_speed ()
-destroyer.show_info ()
-
-cruiser = Cruiser ('Модель корабля: Крейсер.', 'Тип корабля: бойовий корабель.',
-                   'Швидкість: 36 вузлів.', 'Водотоннажність: в середньому 10000 тонн.', 6)
+print()
+frigate.raise_the_sails ('підняти вітрила')
+print ()
+destroyer = Destroyer ('Есмінець.','бойовий корабель.',40,4,'Деякі есмінці мають вертолітний майданчик.')
+destroyer.put_displacement_tonnage ('1500 - 4500 тонн.')
+destroyer.info_about_destroyer ()
+print (' Модель:', destroyer.name)
+print (' Тип:', destroyer.type_of_ship)
+print (str(destroyer))
+print (' Водотоннажність:', destroyer.get_displacement_tonnage())
+print ('Доповнення:\n', destroyer.additions)
+print ()
+cruiser = Cruiser (' Модель: Крейсер.\n','Тип: бойовий корабель.\n','Швидкість: 36 вузлів.\n','Водотоннажність: в середньому 10000 тонн.',6)
 cruiser.get_info ()
-cruiser.add_info ('Такі моделі кораблів є незалежним від загального флоту діючим кораблем.')
+cruiser.add_info ('Особливості:\n Такі моделі кораблів є незалежним від загального флоту діючим кораблем.')
 cruiser.get_info ()
+cruiser.info_about_cruiser ()
 cruiser.show_info ()
